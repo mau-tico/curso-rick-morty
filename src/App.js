490cba1acce51e6efb85467b68390419432df9bc
mau-tico <mauriciorj76@gmail.com>
2022-02-28 02:55:09 -0600

    first commit

---
+++ src/App.js
@@ -0,0 +1,36 @@
+import imageRickMorty from "./img/rick-morty.png";
+import { useState } from "react";
+import "./App.css";
+import { Characters } from "./components/Characters";
+
+function App() {
+  const [characters, setCharacters] = useState(null);
+
+  const reqApi = async () => {
+    const api = await fetch("https://rickandmortyapi.com/api/character");
+    const characterApi = await api.json();
+    setCharacters(characterApi.results);
+  };
+
+  return (
+    <div className="App">
+      <header className="App-header">
+        <h1 className="title">Rick & Morty</h1>
+
+        {characters ? (
+          <Characters characters={characters} setCharacters={setCharacters} />
+        ) : (
+          //{/* Esta seria los props */}
+          <>
+            <img src={imageRickMorty} alt="Rick & Morty" className="img-home" />
+            <button onClick={reqApi} className="btn-search">
+              Buscar Personajes
+            </button>
+          </>
+        )}
+      </header>
+    </div>
+  );
+}
+
+export default App;
